b1b01539595820750bf26603dd3315183a49ac39
gityeop <freiheit517@icloud.com>
2025-07-20 04:38:47 +0900

    Update diskop to v1.0.5

--- Formula/diskop.rb
+++ Formula/diskop.rb
@@ -2,8 +2,8 @@ class Diskop < Formula
   include Language::Python::Virtualenv
   desc "Terminal User Interface (TUI) disk management tool for macOS"
   homepage "https://github.com/gityeop/diskop"
-  url "https://github.com/gityeop/diskop/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "423a5f2bcd830df94caf7f5d5b70918d9a7d8e789c99efeda612de84a89bf08c"
+  url "https://github.com/gityeop/diskop/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "d54c9cdcddf4c586f364a5440dbe5b3a5c59b3961e63b6fd3bff89458bc82d88"
   license "MIT"
 
   depends_on "python@3.11"
